f3fa2ecf5d9863f097d3abbd3bd69fe7b3819c9a
Rayfess <srayfes@gmail.com>
2025-02-22 11:29:58 +0700

    adding alpine nowanels

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,14 @@
 import "./bootstrap";
 import Alpine from "alpinejs";
 
-window.Alpine = Alpine;
+Alpine.data("dropdown", () => ({
+    open: false,
 
+    toggle() {
+        this.open = !this.open;
+        this.open;
+    },
+}));
+
+window.Alpine = Alpine;
 Alpine.start();
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,23 @@
 <x-layout>
   <div x-data="{ open: false }">
     <button @click="open = true">Expand</button>
- 
     <span x-show="open">
         <div class="bg-slate-500 max-w-md">
           <p class="text-8xl">Hello World!</p>
         </div>
     </span>
 </div>
+
+<div x-data="dropdown">
+  <button @click="toggle">Expand</button>
+
+  <span x-show="open">Content...</span>
+</div>
+
+<div x-data="dropdown">
+  <button @click="toggle">Expand</button>
+
+  <span x-show="open">Some Other Content...</span>
+</div>
+
 </x-layout>
